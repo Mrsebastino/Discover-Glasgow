4eab14bd34880fb114f8c536899b9730074885b5
mrsebastino <mrsebastino@gmail.com>
2020-01-23 15:27:34 +0000

    added the three markers for restaurants

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,17 @@
 function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 12,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 55.860916,
+            lng: -4.251433,
         }
     });
     let labels = "ABCDEFGHIJKLMNOPQRSTUVWYXZ";
 
     let locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874256 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 55.87751, lng: -4.2914007 },
+        { lat: 55.8706124, lng: -4.3181257 },
+        { lat: 55.8723885, lng: -4.2847306 }
     ];
     //the function below can take 3 arguments, need to find out what third argument can be
     let markers = locations.map(function (location, i) {
