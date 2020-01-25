b10483c03a5f702e8ac50335803e93ff0bf82aa0
mrsebastino <mrsebastino@gmail.com>
2020-01-25 14:45:44 +0000

    implemeted a toggle function to display top 3 of main 3 section, and aded an intro <p> to the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("hello");
+$(document).ready(function () {
+
+    $(".restaurant-container").click(function () {
+        $(".restaurant-details").toggle();
+    });
+    $(".pub-container").click(function () {
+        $(".pub-details").toggle();
+    });
+    $(".culture-container").click(function () {
+        $(".culture-details").toggle();
+    });
+
+});
