316b495ff89edaf899e275360ae2cd3768bf4eac
mrsebastino <mrsebastino@gmail.com>
2020-01-30 13:52:09 +0000

    trying to implement the link from a button to a marker on the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,42 +1,59 @@
 const locations = [
-    { lat: 55.87751, lng: -4.2914007, name: "Cail Bruich", website: "https://www.cailbruich.co.uk/" },
-    { lat: 55.8706124, lng: -4.3181257, name: "Basta Pizza", website: "https://www.bastapizza.com/ " },
-    { lat: 55.8723885, lng: -4.2847306, name: "Catch Fish & Chips", website: "https://catchfishandchips.co.uk/" },
+    {
+        lat: 55.87751, lng: -4.2914007,
+        name: "Cail Bruich",
+        website: "https://www.cailbruich.co.uk/"
+    },
+
+    {
+        lat: 55.8706124, lng: -4.3181257,
+        name: "Basta Pizza",
+        website: "https://www.bastapizza.com/ "
+    },
+
+    {
+        lat: 55.8723885, lng: -4.2847306,
+        name: "Catch Fish & Chips",
+        website: "https://catchfishandchips.co.uk/"
+    },
 ];
 
 function initMap() {
-    const infowindow = new google.maps.InfoWindow();
-    const map = new google.maps.Map(document.getElementById('map'), {
+    let infowindow = new google.maps.InfoWindow();
+    let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: new google.maps.LatLng(55.860916, -4.251433)
     });
 
-    function placeMarker(loc) {
-        const marker = new google.maps.Marker({
-            position: new google.maps.LatLng(loc.lat, loc.lng),
+    function placeMarker(location) {
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(location.lat, location.lng),
             map: map
         });
 
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.close();
-            infowindow.setContent(`<div id="infowindow">${loc.name}<a href=${loc.website} target="_blank">Website</a></div >`);
+        google.maps.event.addListener(marker, "click", function () {
+            infowindow.setContent(`<div id="infowindow">${location.name}<a href=${location.website} target="_blank">Website</a></div >`);
             infowindow.open(map, marker);
         });
     }
-
+    /* pass every location to place marker*/
     locations.forEach(placeMarker);
 
-    function goToLocation(locationIndex) {
-        let location = locations[locationIndex];/*from here we zoom on location, and open the window, moved it init map*/
-        alert(location)
-    }
+    google.maps.event.addDomListener(window, 'load', initMap);
 
+    /*allow us to zoom to the location on the map*/
+    /* function goToLocation(locationIndex) {
+         let location = locations[locationIndex];
+          google.maps.event.addListener(function () {
+              goToLocation(location);
+          });
+     }*/
 }
 
 function goToLocation(locationIndex) {
-    let location = locations[locationIndex];/*from here we zoom on location, and open the window, moved it init map*/
-    alert(location)
+    let location = locations[locationIndex];
+    /*let mapBasta = document.getElementsByTagName("button");*/
+    document.getElementById("mapBasta").addEventListener("click", function () {
+        goToLocation(map, locations[0, 1]);
+    });
 }
-
-
-/*google.maps.event.addDomListener(window, 'load', initMap);*/
