c45d8d7274a6ffcb0c8313c0d14b1a403b8fa00b
mrsebastino <mrsebastino@gmail.com>
2020-02-04 15:44:40 +0000

    implemented media querie for the 3 section in mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
     //this is to have more information displayed when we click on button
     // one is general info and one is a top 3
+    // the if/else acted as the media queries for mobile view
     $(".restaurant-container").click(function () {
-        $(".restaurantDetails,.recommendedRestaurant").toggle();
+        let rest_cont_detail = window.matchMedia("(min-width:768px)");
+        if (window.matchMedia("(min-width:768px)").matches) {
+            $(".restaurantDetails,.recommendedRestaurant").toggle(1000);
+        } else {
+            $(".recommendedRestaurant").toggle(1000);
+        };
     });
     $(".pub-container").click(function () {
-        $(".pubDetails, .recommendedPubs").toggle();
+        let pub_cont_detail = window.matchMedia("(min-width:768px)");
+        if (window.matchMedia("(min-width:768px)").matches) {
+            $(".pubDetails, .recommendedPubs").toggle(1000);
+        } else {
+            $(".recommendedPubs").toggle(1000);
+        };
     });
     $(".culture-container").click(function () {
-        $(".cultureDetails, .recommendedCulture").toggle();
+        let cult_cont_detail = window.matchMedia("(min-width:768px)");
+        if (window.matchMedia("(min-width:768px)").matches) {
+            $(".cultureDetails, .recommendedCulture").toggle(1000);
+        } else {
+            $(".recommendedCulture").toggle(1000);
+        };
     });
 
-    $("#mapBasta").click(function () {
-        $("#mapBasta").css("background-color", "pink");
-    });
+
 });
 
