a5f64eb8a0e6c6201be39f3cd9e70ff2210b8cf9
mrsebastino <mrsebastino@gmail.com>
2020-02-04 15:35:53 +0000

    added 3 markers in each pub and culture section

--- assets/js/map.js
+++ assets/js/map.js
@@ -16,12 +16,48 @@ const locations = [
         name: "Catch Fish & Chips",
         website: "https://catchfishandchips.co.uk/"
     },
+
+    {
+        lat: 55.8682991, lng: -4.2933792,
+        name: "Brew Dog West End",
+        website: "https://brewdog.com.co.uk/"
+    },
+
+    {
+        lat: 55.8708818, lng: -4.2987629,
+        name: "Lismore",
+        website: "https://www.facebook.com/LismoreBar/?rf=118642888189238/"
+    },
+
+    {
+        lat: 55.8609006, lng: -4.2585135,
+        name: " The Horseshoe Bar",
+        website: "https://www.thehorseshoebarglasgow.co.uk/"
+    },
+
+    {
+        lat: 55.8685921, lng: -4.2927926,
+        name: "The Kelvingrove Gallery",
+        website: "https://www.glasgowlife.org.uk/museums/venues/kelvingrove-art-gallery-and-museum/"
+    },
+
+    {
+        lat: 55.865106, lng: -4.3083949,
+        name: "The Transport Museum",
+        website: "https://www.glasgowlife.org.uk/museums/venues/riverside-museum/"
+    },
+
+    {
+        lat: 55.8601344, lng: -4.2543698,
+        name: "Gallery of Modern Art",
+        website: "https://www.glasgowlife.org.uk/museums/venues/gallery-of-modern-art-goma/"
+    },
 ];
 
 function initMap() {
     let infowindow = new google.maps.InfoWindow();
     let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 13,
         center: new google.maps.LatLng(55.860916, -4.251433)
     });
 
@@ -32,7 +68,7 @@ function initMap() {
         });
 
         google.maps.event.addListener(marker, "click", function () {
-            infowindow.setContent(`<div id="infowindow">${location.name}<a href=${location.website} target="_blank">Website</a></div >`);
+            infowindow.setContent(`<div id="infowindow">${location.name} <a href=${location.website} target="_blank">Website</a></div >`);
             infowindow.open(map, marker);
         });
     }
