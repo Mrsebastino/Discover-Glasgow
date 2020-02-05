71890f00d596fa25f7a264f74c4203355c17910c
mrsebastino <mrsebastino@gmail.com>
2020-02-05 17:02:57 +0000

    testing a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //this is to have more information displayed when we click on button
     // one is general info and one is a top 3
     // the if/else acted as the media queries for mobile view
-    $(".restaurant-container").click(function () {
+    $("#more-food").click(function () {
         let rest_cont_detail = window.matchMedia("(min-width:768px)");
         if (window.matchMedia("(min-width:768px)").matches) {
             $(".restaurantDetails,.recommendedRestaurant").toggle(1000);
@@ -26,7 +26,5 @@ $(document).ready(function () {
             $(".recommendedCulture").toggle(1000);
         };
     });
-
-
 });
 
