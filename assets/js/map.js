9f9c43beed7c8ee3af4d483057d939bd472a5d5c
mrsebastino <mrsebastino@gmail.com>
2020-02-14 16:10:31 +0000

    reverted to original code

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,114 +1,92 @@
 const locations = [
     {
-        coords: { lat: 55.87751, lng: -4.2914007 },
-        content: `<h6>Cail Bruich</h6>
-        website: "https://www.cailbruich.co.uk/"`
-
+        lat: 55.87751, lng: -4.2914007,
+        name: "Cail Bruich",
+        website: "https://www.cailbruich.co.uk/"
     },
 
     {
-        coords: { lat: 55.8706124, lng: -4.3181257 },
-        content: `<h6>Basta Pizza</h6>`,
+        lat: 55.8706124, lng: -4.3181257,
+        name: "Basta Pizza",
         locator: "#mapBasta",
         website: "https://www.bastapizza.com/ "
     },
 
     {
-        coords: { lat: 55.8723885, lng: -4.2847306 },
-        content: `<h6>Catch Fish and Chips</h6>`,
+        lat: 55.8723885, lng: -4.2847306,
+        name: "Catch Fish & Chips",
         website: "https://catchfishandchips.co.uk/"
     },
 
     {
-        coords: { lat: 55.8682991, lng: -4.2933792 },
-        content: `<h6>Brew Dog West End</h6>`,
+        lat: 55.8682991, lng: -4.2933792,
+        name: "Brew Dog West End",
         website: "https://brewdog.com.co.uk/"
     },
 
     {
-        coords: { lat: 55.8708818, lng: -4.2987629 },
-        content: `<h6>Lismore</h6>`,
+        lat: 55.8708818, lng: -4.2987629,
+        name: "Lismore",
         website: "https://www.facebook.com/LismoreBar/?rf=118642888189238/"
     },
 
     {
-        coords: { lat: 55.8609006, lng: -4.2585135 },
-        content: `<h6>The Horseshoe Bar</h6>`,
+        lat: 55.8609006, lng: -4.2585135,
+        name: " The Horseshoe Bar",
         website: "https://www.thehorseshoebarglasgow.co.uk/"
     },
 
     {
-        coords: { lat: 55.8685921, lng: -4.2927926 },
-        content: `<h6>The Kelvingrove Gallery</h6>`,
+        lat: 55.8685921, lng: -4.2927926,
+        name: "The Kelvingrove Gallery",
         website: "https://www.glasgowlife.org.uk/museums/venues/kelvingrove-art-gallery-and-museum/"
     },
 
     {
-        coords: { lat: 55.865106, lng: -4.3083949 },
-        content: `<h6>The Transport Museum</h6>`,
+        lat: 55.865106, lng: -4.3083949,
+        name: "The Transport Museum",
         website: "https://www.glasgowlife.org.uk/museums/venues/riverside-museum/"
     },
 
     {
-        coords: { lat: 55.8601344, lng: -4.2543698 },
-        content: `<h6>Gallery of Modern Art</h6>`,
+        lat: 55.8601344, lng: -4.2543698,
+        name: "Gallery of Modern Art",
         website: "https://www.glasgowlife.org.uk/museums/venues/gallery-of-modern-art-goma/"
     },
 ];
 
-// load the map onto our page
 function initMap() {
+    let infowindow = new google.maps.InfoWindow();
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: new google.maps.LatLng(55.860916, -4.251433)
     });
 
-    // loop through marker
-    for (let i = 0; i < locations.length; i++) {
-        // add marker
-        placeMarker(locations[i]);
-    }
-
-    // define the marker
-    function placeMarker(props) {
+    function placeMarker(location) {
         let marker = new google.maps.Marker({
-            position: props.coords,
+            position: new google.maps.LatLng(location.lat, location.lng),
             map: map
         });
 
-        //Check content
-        if (props.content) {
-            let infowindow = new google.maps.InfoWindow({
-                content: props.content
-            });
-
-
-            // event that display the infowindow
-            marker.addListener("click", function () {
-                infowindow.open(map, marker);
-            });
-        }
-
-        /* add some info in infowindow   
-        let infowindow = new google.maps.InfoWindow();
         google.maps.event.addListener(marker, "click", function () {
-            infowindow.setContent(`<div id="infowindow">${locations.name} <a href=${locations.website} target="_blank">Website</a></div >`);
+            infowindow.setContent(`<div id="infowindow">${location.name} <a href=${location.website} target="_blank">Website</a></div >`);
             infowindow.open(map, marker);
-        });*/
-
-        /*need to build an if statement to work with locator
-        $("#mapBasta").click(function (e) {
+        });
+        /*need to build an if statement to work with locator*/
+        $("#mapBasta").click(function () {
             google.maps.event.trigger("#mapBasta", "click");
-            infowindow.setContent(`<div id="infowindow">${locations.name} <a href=${locations.website} target="_blank">Website</a></div >`);
+            infowindow.setContent(`<div id="infowindow">${location.name} <a href=${location.website} target="_blank">Website</a></div >`);
             infowindow.open(map, marker);
-        });*/
-
+        });
     }
 
+    /*pass every location to place marker*/
+    locations.forEach(placeMarker);
 
     google.maps.event.addDomListener(window, 'load', initMap);
+
 }
 
-setTimeout(function () {
-    google.maps.event.trigger("#mapBasta", "click");
-}, 1000);
+/* google.maps.event.trigger(marker, 'click');
+            infowindow.setContent(`<div id="infowindow">${locations.name} <a href=${location.website} target="_blank">Website</a></div >`);
+            infowindow.open(map, marker);*/
