df1a151cf4690843fd86fcce5573fb1889306b2a
mrsebastino <mrsebastino@gmail.com>
2020-03-11 16:47:05 +0000

    changed the name of buttons to avoid repetitions of ID

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,28 +2,28 @@
 $(document).ready(function () {
     //this is to have more information displayed when we click on the button
     // the if/else acted as the media queries for mobile view inpired from w3 school
-    $("#more-food").click(function () {
+    $("#moreFoodButton").click(function () {
         let rest_cont_detail = window.matchMedia("(min-width:768px)");
         if (window.matchMedia("(min-width:768px)").matches) {
             $(".restaurantDetails,.recommendedRestaurant").toggle(1000);
         } else {
-            $(".recommendedRestaurant").toggle(1000);
+            $(".restaurantDetails,.recommendedRestaurant").toggle(1000);
         };
     });
-    $("#more-drink").click(function () {
+    $("#moreDrinkButton").click(function () {
         let pub_cont_detail = window.matchMedia("(min-width:768px)");
         if (window.matchMedia("(min-width:768px)").matches) {
             $(".pubDetails, .recommendedPubs").toggle(1000);
         } else {
-            $(".recommendedPubs").toggle(1000);
+            $(".pubDetails,.recommendedPubs").toggle(1000);
         };
     });
-    $("#more-art").click(function () {
+    $("#moreArtButton").click(function () {
         let cult_cont_detail = window.matchMedia("(min-width:768px)");
         if (window.matchMedia("(min-width:768px)").matches) {
             $(".cultureDetails, .recommendedCulture").toggle(1000);
         } else {
-            $(".recommendedCulture").toggle(1000);
+            $(".cultureDetails,.recommendedCulture").toggle(1000);
         };
     });
 
@@ -35,9 +35,8 @@ $(document).ready(function () {
 
 })
 
-
 // Close the dropdown menu if the user clicks outside of it
-/*window.onclick = function (event) {
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
@@ -48,7 +47,7 @@ $(document).ready(function () {
             }
         }
     }
-}*/
+}
 
 
 
